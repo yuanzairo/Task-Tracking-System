93cf8d4fff4e7eef62cc5285462cdb2493974809
yuanzairo <yuan.zairo@gmail.com>
2026-05-27 07:00:10 +0800

    Update database.config.php

--- public/database.config.php
+++ public/database.config.php
@@ -4,4 +4,4 @@ $SERVER_NAME = getenv('SERVER_NAME');
 $USERNAME    = getenv('USERNAME');
 $PASSWORD    = getenv('PASSWORD');
 $DB_NAME     = getenv('DB_NAME');
-$PORT        = (int)getenv('PORT');
+$PORT        = 3306
